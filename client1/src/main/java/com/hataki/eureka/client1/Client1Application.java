ba88208a291240158d951f31d3e513556d0a8a30
wangmiao <wangiao@medicinovo.com>
2020-07-15 17:46:28 +0800

    client get eureka server

--- client1/src/main/java/com/hataki/eureka/client1/Client1Application.java
+++ client1/src/main/java/com/hataki/eureka/client1/Client1Application.java
@@ -2,12 +2,21 @@ package com.hataki.eureka.client1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Client1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Client1Application.class, args);
+    }
+
+
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
     }
 
 }
--- client1/src/main/java/com/hataki/eureka/client1/MainController.java
+++ client1/src/main/java/com/hataki/eureka/client1/MainController.java
@@ -1,9 +1,14 @@
 package com.hataki.eureka.client1;
 
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -13,16 +18,21 @@ import java.util.List;
  * Time: 13:57
  * description:
  */
-@Controller
+@RestController
 public class MainController {
 
-    @Autowired
     /**
      * cause of netflex closed source of eureka
      * springcloud supplied a new abstract interface
      *
      * */
+    @Autowired
     private DiscoveryClient client ;
+
+    @Autowired
+    private EurekaClient eurekaClient ;
+
+
 
     @GetMapping("/hi")
     public String getHi(){
@@ -31,11 +41,27 @@ public class MainController {
 
     @GetMapping("/client")
     public String getClient(){
+
         List<String> services = client.getServices();
         for(String str : services){
             System.out.println(str);
         }
         return "Service!";
+    }
+
+
+    @GetMapping("/client2")
+    public String getClient2() {
+
+        List<InstanceInfo> eurekaServer = eurekaClient.getInstancesByVipAddress("EurekaServer", false);
+
+
+        for (InstanceInfo instanceInfo : eurekaServer) {
+
+            System.out.println(instanceInfo.getStatus());
+            String s = "http://" + instanceInfo.getHostName() + ":" + instanceInfo.getPort() + "/eureka";
+        }
+        return "Service2!";
     }
 
 
